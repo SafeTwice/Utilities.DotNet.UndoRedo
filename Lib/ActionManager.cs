77dc7349b2ff98a5661296bc5d7d9099c504a189
Jesús González <gonzalez.jesus@safetwice.com>
2022-02-09 02:56:01 +0100

    Initial import

---
+++ Lib/ActionManager.cs
@@ -0,0 +1,134 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2022 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+
+namespace UndoRedoFramework
+{
+    /// <summary>
+    /// Manages actions that can be done and undone.
+    /// </summary>
+    public class ActionManager
+    {
+        //===========================================================================
+        //                           PUBLIC PROPERTIES
+        //===========================================================================
+
+        public string? UndoActionDescription => m_undoActions.Last?.Value.Description;
+
+        public string? RedoActionDescription => m_redoActions.Last?.Value.Description;
+
+        //===========================================================================
+        //                             PUBLIC EVENTS
+        //===========================================================================
+
+        public event EventHandler? UndoRedoStateChanged;
+
+        //===========================================================================
+        //                          PUBLIC CONSTRUCTORS
+        //===========================================================================
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <remarks>
+        /// When the number of stored actions to be undone exceeds <paramref name="maxUndoSize"/>, older actions to be
+        /// undone are deleted.
+        /// </remarks>
+        /// <param name="maxUndoSize">Max size of actions to be undone that are stored</param>
+        public ActionManager( uint maxUndoSize = uint.MaxValue )
+        {
+            m_maxActions = maxUndoSize;
+        }
+
+        //===========================================================================
+        //                            PUBLIC METHODS
+        //===========================================================================
+
+        /// <summary>
+        /// Executes an action and stores it in the undo queue.
+        /// </summary>
+        /// <param name="action">Action to be executed</param>
+        public void Execute( IAction action )
+        {
+            action.Execute();
+
+            if( m_undoActions.Last?.Value.TryMerge( action ) != true )
+            {
+                m_undoActions.AddLast( action );
+
+                if( m_undoActions.Count > m_maxActions )
+                {
+                    m_undoActions.RemoveFirst();
+                }
+
+                m_redoActions.Clear();
+            }
+
+            UndoRedoStateChanged?.Invoke( this, new() );
+        }
+
+        /// <summary>
+        /// Unexecutes the newest action in the undo queue and moves it to the redo queue.
+        /// </summary>
+        public void Undo()
+        {
+            var action = m_undoActions.Last?.Value;
+
+            if( action != null )
+            {
+                action.UnExecute();
+
+                m_undoActions.RemoveLast();
+                m_redoActions.AddLast( action );
+
+                UndoRedoStateChanged?.Invoke( this, new() );
+            }
+        }
+
+        /// <summary>
+        /// Indicates if an action is available to be undone.
+        /// </summary>
+        /// <returns><c>true</c> if an action to be undone is available, <c>false</c> otherwise</returns>
+        public bool CanUndo()
+        {
+            return m_undoActions.Count > 0;
+        }
+
+        /// <summary>
+        /// Re-executes the oldest action in the redo queue and moves it to the undo queue.
+        /// </summary>
+        public void Redo()
+        {
+            var action = m_redoActions.Last?.Value;
+
+            if( action != null )
+            {
+                action.Execute();
+
+                m_redoActions.RemoveLast();
+                m_undoActions.AddLast( action );
+
+                UndoRedoStateChanged?.Invoke( this, new() );
+            }
+        }
+
+        /// <summary>
+        /// Indicates if an action is available to be redone.
+        /// </summary>
+        /// <returns><c>true</c> if an action to be redone is available, <c>false</c> otherwise</returns>
+        public bool CanRedo()
+        {
+            return m_redoActions.Count > 0;
+        }
+
+        //===========================================================================
+        //                           PRIVATE ATTRIBUTES
+        //===========================================================================
+
+        private LinkedList<IAction> m_undoActions = new();
+        private LinkedList<IAction> m_redoActions = new();
+        private uint m_maxActions;
+    }
+}
---
+++ Lib/IAction.cs
@@ -0,0 +1,30 @@
+﻿/// @file
+/// @copyright  Copyright (c) 2022 SafeTwice S.L. All rights reserved.
+/// @license    See LICENSE.txt
+
+using System;
+
+namespace UndoRedoFramework
+{
+    /// <summary>
+    /// Represents an action than can be executed (done/redone) and unexecuted (undone).
+    /// </summary>
+    public interface IAction
+    {
+        //===========================================================================
+        //                              PROPERTIES
+        //===========================================================================
+
+        string Description { get; }
+
+        //===========================================================================
+        //                               METHODS
+        //===========================================================================
+
+        void Execute();
+
+        void UnExecute();
+
+        bool TryMerge( IAction action );
+    }
+}
